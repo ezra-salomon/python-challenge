bf4c6975d0988bde97dc8d5dbfb8c6bff9e06dd1
Ezra Salomon <ezsalo@gmail.com>
2023-09-23 10:56:31 -0700

    Finished version

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,35 +3,63 @@ import os
 import csv
 
 #constants
-totalMonths = 0
+totalMonths = 1
 totalProf = 0
-aveChange = totalProf % 86
-#greatProf = 0
+totalChange = 0
+chgAve = 0
+dateGreat = " "
+dateMin = " "
+greatProf = 0
+greatLoss = 0
 
 
 #working code
 csvpath = os.path.join('/Users/ezrasalomon/Desktop/Bootcamp/Python/python-challenge/PyBank', 'Resources', 'budget_data.csv')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+
     #print(csvreader)
-    
     header = next(csvreader)
+    sec_row = next(csvreader)
+    prev_row = next(csvreader)
+    totalMonths += 1
+    totalProf += int(prev_row[1])
+    totalProf += int(sec_row[1])
+    totalChange += int(prev_row[1]) - int(sec_row[1])
+    greatProf += int(prev_row[1]) - int(sec_row[1])
+    greatLoss += int(prev_row[1]) - int(sec_row[1])
+    dateGreat = prev_row[0]
+    dateMin = prev_row[0]
+
     for row in csvreader:
         totalMonths += 1
         totalProf += int(row[1])
-        #aveChange totalProf % 86
+        totalChange += int(row[1]) - int(prev_row[1])
+        
 
-        #greatProf += max(int(row[1]))
+        if int(row[1]) - int(prev_row[1]) > greatProf:
+            greatProf = int(row[1]) - int(prev_row[1])
+            dateGreat = row[0]
 
-        #print(int(row[1]))
-        #row[0] += str(totalMonths)
+        if int(row[1]) - int(prev_row[1]) < greatLoss:
+            greatLoss = int(row[1]) - int(prev_row[1])
+            dateMin = row[0]
+            
+
+
+
+        prev_row = row
+        
+chgAve = totalChange / (totalMonths - 1)
+         
+        
 #output
 print("Financial Analysis")
 print(f"Total Months: {totalMonths} ")
 print(f"Total: ${totalProf}")
-print(f"Average Change: ${aveChange}")
-#print(f"Greatest Increase in Profits: {greatProf}")
-
+print(f"Total Change: ${chgAve}")
+print(f"Greatest Increase in Profits:{dateGreat}({greatProf})")
+print(f"Greatest Loss in Profits:{dateMin}({greatLoss})")
 
 
 
