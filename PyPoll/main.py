dca4a85cec5d1ae94a162f7ac3d7ea83e5f9fa7a
Ezra Salomon <ezsalo@gmail.com>
2023-09-23 15:10:54 -0700

    All logic except percentages

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,6 +2,7 @@
 import os
 import csv
 
+#variable assignments
 totalVotes = 0
 ccs = "Charles Casper Stockham"
 ccsVts = 0
@@ -10,27 +11,32 @@ ddgVts = 0
 rad = "Raymon Anthony Doane"
 radVts = 0 
 winner = " "
+percVtsC = 0
+percVtsD = 0
+percVtsR = 0
 
 csvpath = os.path.join('/Users/ezrasalomon/Desktop/Bootcamp/Python/python-challenge/PyPoll', 'Resources', 'election_data.csv')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
-    #ccsNumVts = next(csvreader)
-    #nameCount = next(csvreader)
+    
 
 
-
+#iterate through csv file
     for row in csvreader:
         totalVotes += 1
 
-        #ccsVts += row[2]
-
+        
+#determin number and percentage of votes for each candidates
         if row[2] == ccs:
             ccsVts += 1
+            percVtsC = (ccsVts / totalVotes * 100)
         elif row[2] == ddg:
             ddgVts += 1
+            percVtsD = (ddgVts / totalVotes * 100)
         else: radVts += 1
-
+        percVtsR = (radVts / totalVotes * 100)
+#Determine winner
         if ccsVts > ddgVts + radVts:
             winner = ccsVts
         elif ddgVts > ccsVts + radVts:
@@ -38,7 +44,7 @@ with open(csvpath) as csvfile:
         elif radVts > ccsVts + ddgVts:
             winner = radVts
         
-        #if nameCount(str(row[2])).count(nameCount):
+        
 
 
 
@@ -47,7 +53,7 @@ with open(csvpath) as csvfile:
 
 print("Election Results")
 print(f"Total votes:{totalVotes}")
-print(f"Charles Casper Stockham:({ccsVts})")
-print(f"Diana DeGette:({ddgVts})")
-print(f"Raymon Anthony Doane:({radVts})")
-print(f"Winner:{winner}")
+print(f"Charles Casper Stockham:{percVtsC}% ({ccsVts})")
+print(f"Diana DeGette:{percVtsD}% ({ddgVts})")
+print(f"Raymon Anthony Doane:{percVtsR}% ({radVts})")
+print(f"Winner: {winner}")
